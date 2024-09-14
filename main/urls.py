2ff6d1a09696da06040d51a3b05ce507f327d89e
Egor Lavysh <egorlavys95@gmail.com>
2024-09-14 16:59:56 +0300

    system filtration of apps

--- main/urls.py
+++ main/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("petsitters/", views.PetsittersListView.as_view(), name="show_petsitters"),
     path("<str:username>/", views.user_profile, name="user_profile"),
-    path("<str:username>/applications/", views.ApplicationsListView.as_view(), name="applications"),
+    path("<str:username>/applications/", views.ApplicationsListView, name="applications"),
     path("<str:username>/applications/<int:pk>", views.ApplicationDetailView.as_view(), name="application_detail"),
 ]
--- main/views.py
+++ main/views.py
@@ -4,7 +4,7 @@ from users.models import User, Petsitter
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from orders.models import Order
-
+from django.db.models import Q
 
 def index(request):
     return HttpResponse("The main page")
@@ -24,13 +24,39 @@ def user_profile(request, username):
     else:
         return HttpResponse("У вас нет доступа к профилю")
     
-class ApplicationsListView(ListView):
-    model=Order
-    template_name="main/applications_list.html"
+#class ApplicationsListView(ListView):
+#    model=Order
+#    template_name="main/applications_list.html"
 
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        return queryset.filter(owner=self.request.user)
+#    def get_queryset(self):
+#        queryset = super().get_queryset()
+#        if self.request.user.is_petsitter:
+#            return queryset.filter(Q(owner=self.request.user) | Q(petsitter=self.request.user))
+#        else:
+#            return queryset.filter(owner=self.request.user)
+
+def ApplicationsListView(request, username):
+    if request.user.username == username:
+        if request.user.is_petsitter:
+            filter_apps = request.GET.get("filter")
+
+            if filter_apps == "petsitter":
+                context = {
+                    "object_list" : Order.objects.filter(petsitter=request.user)
+                }
+            elif filter_apps == "all":
+                context = {
+                    "object_list" : Order.objects.filter(Q(petsitter=request.user) | Q(owner=request.user))
+                }
+            else:
+                context = {
+                    "object_list" : Order.objects.filter(owner=request.user)
+                }
+            return render(request, "main/applications_list.html", context=context)
+        return render(request, "main/applications_list.html", context={"object_list" : Order.objects.filter(owner=request.user)})
+    else: 
+        return HttpResponse("У вас нет доступа.")
+
     
 class ApplicationDetailView(DetailView):
     model = Order
