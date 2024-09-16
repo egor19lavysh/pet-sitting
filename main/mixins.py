aad11f1d38322c66368cd1daa5fd51cad26ed713
Egor Lavysh <egorlavys95@gmail.com>
2024-09-16 23:18:44 +0300

    some changes in Order

--- main/mixins.py
+++ main/mixins.py
@@ -6,6 +6,6 @@ from django.http import HttpResponseForbidden
 class OrderOwnerPetsitterRequiredMixin:
     def dispatch(self, request, *args, **kwargs):
         order = get_object_or_404(Order, pk=kwargs['pk'])
-        if order.owner != request.user or order.petsitter != request.user:
+        if order.owner != request.user and order.petsitter != request.user:
             return HttpResponseForbidden("У вас нет доступа к этой заявке")
         return super().dispatch(request, *args, **kwargs)
