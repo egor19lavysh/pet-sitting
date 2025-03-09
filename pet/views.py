a9b9b8768ca8e0707f38074f398b10afc561d5c0
Egor Lavysh <egorlavys95@gmail.com>
2025-03-10 00:36:09 +0300

    refactored pet views

--- pet/views.py
+++ pet/views.py
@@ -1,7 +1,6 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, UpdateView, DeleteView, DetailView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.shortcuts import redirect
 from .models import Pet
 from .forms import PetForm
 from .mixins import PetOwnerRequiredMixin
@@ -11,6 +10,7 @@ class PetCreateView(LoginRequiredMixin, CreateView):
     form_class = PetForm
     template_name = "pet/create.html"
     success_url = reverse_lazy("main:index")
+    login_url = reverse_lazy("users:login")
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
@@ -19,19 +19,22 @@ class PetCreateView(LoginRequiredMixin, CreateView):
 class PetDetailView(LoginRequiredMixin, PetOwnerRequiredMixin, DetailView):
     model = Pet
     template_name = "pet/show.html"
+    login_url = reverse_lazy("users:login")
 
 class PetUpdateView(LoginRequiredMixin, PetOwnerRequiredMixin, UpdateView):
     model = Pet
     form_class = PetForm
     template_name = "pet/update.html"
+    login_url = reverse_lazy("users:login")
 
     def get_success_url(self):
-        return reverse_lazy("pet:detail", kwargs={"pk": self.object.id})
+        return reverse_lazy("pet:read_pet", kwargs={"pk": self.object.id})
 
 class PetDeleteView(LoginRequiredMixin, PetOwnerRequiredMixin, DeleteView):
     model = Pet
     template_name = "pet/delete.html"
     success_url = reverse_lazy("main:index")
+    login_url = reverse_lazy("users:login")
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
